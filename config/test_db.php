525bc7c19d141814b59d30b4639fb9b68d6735bb
bitsnaps <bitsnaps@yahoo.fr>
2021-01-11 08:53:35 +0100

    update travis for fix DB test

--- config/test_db.php
+++ config/test_db.php
@@ -2,5 +2,6 @@
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
 $db['dsn'] = 'mysql:host=127.0.0.1:3306;dbname=todo_test';
+$db['username'] = 'root'; // for travis-ci use: 'root' or 'travis' for less privileges.
 
 return $db;
