df3af70d29094b4513536493c7752e6290aa80a0
Ibrahim.H <bitsnaps@yahoo.fr>
2021-01-13 21:19:50 +0100

    update composer to enable acceptance tests

---
+++ acceptance.sh
@@ -0,0 +1,5 @@
+# for Firefox (not working for me)
+# java -jar -Dwebdriver.gecko.driver=/full/path/to/selenium/geckodriver /Users/mac/Public/Dev/selenium/selenium-server-standalone-3.141.59.jar
+
+# for Google Chrome
+java -jar -Dwebdriver.chrome.driver=/full/path/to/selenium/chromedriver /Users/mac/Public/Dev/selenium/selenium-server-standalone-3.141.59.jar
