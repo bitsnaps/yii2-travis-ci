c048e53542767a789d3430dc438e45e9f4ebc669
Ibrahim.H <bitsnaps@yahoo.fr>
2021-01-15 12:26:16 +0100

    fix: syntax error in config

--- config/test_db.php
+++ config/test_db.php
@@ -2,8 +2,7 @@
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
 // $db['dsn'] = 'mysql:host=127.0.0.1:3306;dbname=todo_test';
-$db['class'] => 'yii\db\Connection';
-
+$db['class'] = 'yii\db\Connection';
 $db['dsn'] = 'pgsql:host=localhost;dbname=todo_test';
 $db['username'] = 'postgres';
 $db['password'] = '';
