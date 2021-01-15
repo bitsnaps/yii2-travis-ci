f664165a31457a0a14ec8e35ed03518df2fab593
Ibrahim.H <bitsnaps@yahoo.fr>
2021-01-15 12:19:18 +0100

    fix: postgresq test confgiuration

--- config/test_db.php
+++ config/test_db.php
@@ -1,7 +1,14 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=127.0.0.1:3306;dbname=todo_test';
-$db['username'] = 'root'; // for travis-ci use: 'root' or 'travis' for less privileges.
+// $db['dsn'] = 'mysql:host=127.0.0.1:3306;dbname=todo_test';
+$db['class'] => 'yii\db\Connection';
+
+$db['dsn'] = 'pgsql:host=localhost;dbname=todo_test';
+$db['username'] = 'postgres';
+$db['password'] = '';
+$db['charset'] = 'utf8'
+
+ // for travis-ci use: 'root' or 'travis' for less privileges.
 
 return $db;
