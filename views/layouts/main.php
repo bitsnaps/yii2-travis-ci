64222e13e0a233cb260ba59855bfc4841028a2da
Ibrahim.H <bitsnaps@yahoo.fr>
2021-01-20 15:08:03 +0100

    add users menu item

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,6 +40,7 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Users', 'url' => ['/user/index'], 'visible' => (Yii::$app->user && !Yii::$app->user->isGuest)],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
